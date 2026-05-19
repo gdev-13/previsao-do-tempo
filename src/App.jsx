930cac57e4c8608ec5b5e61add16b22be6008b96
Geovana Gonçalo <mariageovana82268@gmail.com>
2026-05-19 10:17:33 -0300

    feat: adicionar referência ao campo de busca

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const inputRef = useRef()
+
+  function searchCity() {
+    console.log(inputRef.current.value)
+  }
 
   return (
     <div>
       <h1>Previsão do Tempo</h1>
-      <input type="text" placeholder="Digite a cidade" />
-      <button>Buscar</button>
+      <input ref={inputRef} type="text" placeholder="Digite a cidade" />
+      <button onClick={searchCity}>Buscar</button>
     </div>
   )
 }
