c548b9a6666ec4d402be3eeb27688e863373bebe
Geovana Gonçalo <mariageovana82268@gmail.com>
2026-05-19 11:13:19 -0300

    fix: ajustar parâmetros da API de clima (lang e units)

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   async function searchCity() {
     const city = inputRef.current.value
     const apiKey = import.meta.env.VITE_API_KEY
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&lang=pt_br&units=metric`
 
     const response = await axios.get(apiUrl)
 
