6cec24e85d44005beb299956a1b1eb0677e92bb1
Geovana Gonçalo <mariageovana82268@gmail.com>
2026-05-20 11:39:20 -0300

    feat: preparar aplicação para previsão dos próximos dias

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,20 @@ import Weather from './components/Weather/Weather'
 
 function App() {
   const [weather, setWeather] = useState()
+  const [weatherForecast, setWeatherForecast] = useState()
   const inputRef = useRef()
 
   async function searchCity() {
     const city = inputRef.current.value
     const apiKey = import.meta.env.VITE_API_KEY
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&lang=pt_br&units=metric`
+    const apiUrlForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&lang=pt_br&units=metric`
 
     const response = await axios.get(apiUrl)
+    const responseForecast = await axios.get(apiUrlForecast)
+
     setWeather(response.data)
+    setWeatherForecast(responseForecast.data)
   }
 
   return (
